cbbd3d9947888531592f9f7bb376a3142c605ef5
Andrea <andrea.mazzilli@andrewm.net>
2024-09-29 15:30:30 +0200

    initial draft

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/*
+async function getData() {
+    const url = "test.txt";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const data = await response.data();
+      console.log(data);
+
+      console.log('second fetch');
+      document.querySelector("main").append(data);
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+*/
+
+function load (item){
+    console.log(item);
+    let main = document.querySelector("#main");
+
+    //fetch('test.html')
+    fetch('data/'+item+'.html')
+        .then(response => response.text())
+        .then((data1) => {
+            console.log('first fetch');
+            console.log(data1);
+            main.innerHTML += data1
+            })
+      
+      //console.log('second fetch outside');
+      //console.log(data);
+      //document.querySelector("main").append(data)
+      toggleDrawer();
+}
+
+
+    //let navItemProgetti = document.querySelector('#progetti'); //Nav Layer
+    let navItemProgetti = document.getElementsByName('progetti'); //Nav Layer
+    document.querySelector('progetti').addEventListener('click', ()=>{
+        alert(navItemProgetti);
+    })
